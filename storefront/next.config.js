9990916fe593d0d8302ec2422ffc3ef076e9f94c
nawazmoin <moin.nawazzy@gmail.com>
2025-12-25 03:53:23 +0100

    Fix next.config.js for Vercel deployment

--- storefront/next.config.js
+++ storefront/next.config.js
@@ -18,33 +18,41 @@ const nextConfig = {
       {
         protocol: "http",
         hostname: "localhost",
-        
       },
-      { // Note: needed to serve images from /public folder
-        protocol: process.env.NEXT_PUBLIC_BASE_URL?.startsWith('https') ? 'https' : 'http',
-        hostname: process.env.NEXT_PUBLIC_BASE_URL?.replace(/^https?:\/\//, ''),
-      },
-      { // Note: only needed when using local-file for product media
+      // Only add if NEXT_PUBLIC_BASE_URL is defined
+      ...(process.env.NEXT_PUBLIC_BASE_URL ? [{
+        protocol: process.env.NEXT_PUBLIC_BASE_URL.startsWith('https') ? 'https' : 'http',
+        hostname: process.env.NEXT_PUBLIC_BASE_URL.replace(/^https?:\/\//, ''),
+      }] : []),
+      // Only add if NEXT_PUBLIC_MEDUSA_BACKEND_URL is defined
+      ...(process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL ? [{
         protocol: "https",
-        hostname: process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL?.replace('https://', ''),
-      },
-      { // Note: can be removed after deleting demo products
+        hostname: process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL.replace('https://', ''),
+      }] : []),
+      {
         protocol: "https",
         hostname: "medusa-public-images.s3.eu-west-1.amazonaws.com",
       },
-      { // Note: can be removed after deleting demo products
+      {
         protocol: "https",
         hostname: "medusa-server-testing.s3.amazonaws.com",
       },
-      { // Note: can be removed after deleting demo products
+      {
         protocol: "https",
         hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
       },
-      ...(process.env.NEXT_PUBLIC_MINIO_ENDPOINT ? [{ // Note: needed when using MinIO bucket storage for media
+      // Cloudflare R2
+      {
+        protocol: "https",
+        hostname: "pub-*.r2.dev",
+      },
+      // MinIO/Railway
+      ...(process.env.NEXT_PUBLIC_MINIO_ENDPOINT ? [{
         protocol: "https",
         hostname: process.env.NEXT_PUBLIC_MINIO_ENDPOINT,
       }] : []),
     ],
+    formats: ['image/webp', 'image/avif'],
   },
   serverRuntimeConfig: {
     port: process.env.PORT || 3000
